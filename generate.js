cdabefdfd80f26ae12bf03a008ff8b1cdc87dba5
ryan-bennett <ryan.bennett.au@gmail.com>
2024-08-20 14:24:25 +1000

    fix: aligning local and github builds

--- generate.js
+++ generate.js
@@ -1,3 +1,3 @@
 const {execSync} = require('child_process');
 
-execSync(`docker run --rm -v "${process.cwd()}:/local\" openapitools/openapi-generator-cli:v7.8.0 generate -i /local/spec.json -g typescript-fetch -o /local/generated/typescript-fetch -p supportsES6=true -p npmName=awx-api-client`);
+execSync(`docker run --rm -v "${process.cwd()}:/local\" openapitools/openapi-generator-cli:v7.8.0 generate -i /local/spec.json -g typescript-fetch -o /local/typescript-fetch-client -p supportsES6=true -p npmName=awx-api-client`);
