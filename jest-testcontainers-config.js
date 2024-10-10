7f03bda57f4b3bc8364e8f4cf7d7cdaf98955644
Nick Pang <nick95927@gmail.com>
2024-10-10 13:50:44 +1000

    Run prettier on jest-container-config

--- jest-testcontainers-config.js
+++ jest-testcontainers-config.js
@@ -1,16 +1,23 @@
 module.exports = {
-    awx: {
-        image: 'wiremock/wiremock:3.9.1',
-        ports: [8080],
-        resourcesQuota: {
-            memory: 0.25
-        },
-        bindMounts: [{
-            source: `${__dirname}/.awx`,
-            target: '/home/wiremock',
-            mode: 'rw'
-        }],
-        command: ['--verbose', '--disable-banner', '--record-mappings',
-            '--proxy-all', 'https://host.docker.internal:8043' ]
-    }
-}
+  awx: {
+    image: "wiremock/wiremock:3.9.1",
+    ports: [8080],
+    resourcesQuota: {
+      memory: 0.25,
+    },
+    bindMounts: [
+      {
+        source: `${__dirname}/.awx`,
+        target: "/home/wiremock",
+        mode: "rw",
+      },
+    ],
+    command: [
+      "--verbose",
+      "--disable-banner",
+      "--record-mappings",
+      "--proxy-all",
+      "https://host.docker.internal:8043",
+    ],
+  },
+};
