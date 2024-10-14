0cf181d85886262a0e7d0c686976ea78bf1bb505
Nick Pang <nick95927@gmail.com>
2024-10-14 10:55:13 +1000

    Fixed import & added test units

--- test/ApiApi.test.ts
+++ test/ApiApi.test.ts
@@ -1,11 +1,21 @@
-
+import {
+  ApiApi,
+  ApiJobsListRequest,
+  ApiJobsDeleteRequest,
+  ApiJobTemplatesCreateRequest,
+  ApiJobsReadRequest,
+  ApiJobTemplatesDeleteRequest,
+  ApiJobTemplatesLaunchCreateRequest,
+  JobTemplate,
+  JobLaunch,
+  ResponseError,
+} from "../generated/src";
 import { configuration } from "./Config";
-import {ApiApi} from "../generated/src";
 
 describe("AWX API Test", () => {
   const api = new ApiApi(configuration);
 
-  test("api list", async () => {
+  test("List APIs", async () => {
     const response = await api.apiList();
     expect(response).toEqual({
       description: "AWX REST API",
@@ -17,5 +27,36 @@ describe("AWX API Test", () => {
       login_redirect_override: "",
       swagger: "/api/swagger/",
     });
+  });
+
+  test("List jobs", async () => {
+    const reqParams: ApiJobsListRequest = {
+      version: "v2",
+    };
+    const response = await api.apiJobsList(reqParams);
+    expect(response.count).toBeGreaterThanOrEqual(0);
+    expect(response.next).toEqual(undefined);
+    expect(response.previous).toEqual(undefined);
+  });
+
+  // Fail tests
+  test("Job Delete fail test", async () => {
+    const reqParams: ApiJobsDeleteRequest = {
+      version: "v2",
+      id: "1500000",
+    };
+
+    await expect(api.apiJobsDelete(reqParams)).rejects.toThrow(ResponseError);
+  });
+
+  test("Job template fail test", async () => {
+    const reqParams: ApiJobTemplatesDeleteRequest = {
+      version: "v2",
+      id: "130000",
+    };
+
+    await expect(api.apiJobTemplatesDelete(reqParams)).rejects.toThrow(
+      ResponseError,
+    );
   });
 });
